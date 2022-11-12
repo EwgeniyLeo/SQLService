9cc885124fe5a91e20f01d6f500bbf4dc3b7c4c3
Ewgeniy Leontev <esleontev@mail.ru>
2022-11-12 16:53:33 +0300

    Modify Project

--- src/main/java/ru/netology/sqr/SQLService.java
+++ src/main/java/ru/netology/sqr/SQLService.java
@@ -1,14 +1,13 @@
 package ru.netology.sqr;
 
-public class SQLCalculate {
-    int totalCount = 0;
-
+public class SQLService {
     public int square(int minBorder, int maxBorder) {
+        int totalCount = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= minBorder && i * i <= maxBorder)
+            if (i * i >= minBorder && i * i <= maxBorder){
                 totalCount++;
+            }
         }
         return totalCount;
     }
 }
-
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,11 +1,13 @@
 package ru.netology.sqr;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
-    @org.junit.jupiter.api.Test
+    @Test
     void testSql() {
-        SQLCalculate sql = new SQLCalculate();
+        SQLService sql = new SQLService();
 
         int minBorder = 200;
         int maxBorder = 300;
